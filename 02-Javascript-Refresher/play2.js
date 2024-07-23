875419d9b3a060cf1b49efdb83b371c0fe215747
mormille <30348435+mormille@users.noreply.github.com>
2024-07-23 15:36:14 +0900

    Module 2 complete

--- 02-Javascript-Refresher/play2.js
+++ 02-Javascript-Refresher/play2.js
@@ -7,25 +7,61 @@ const person = {
     }
 };
 
-const person2 = {
-    name: 'Tokugawa',
-    age: 65,
-    greet: function() {
-        console.log('Hi, I am ' + this.name)
-    }
-};
+const printName = (personData) => {
+    console.log(personData.name);
+}
 
-// person.greet();
-// person2.greet();
+printName(person);
 
-//ARRAYS
-const hobbies = ['Sports','Cooking']
-// for (let hobby of hobbies) {
-//     console.log(hobby);
+const printName2 = ({ name }) => {
+    console.log(name);
+}
+
+printName2(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+// const person2 = {
+//     name: 'Tokugawa',
+//     age: 65,
+//     greet: function() {
+//         console.log('Hi, I am ' + this.name)
+//     }
+// };
+
+// // person.greet();
+// // person2.greet();
+
+// //ARRAYS
+const hobbies = ['Sports','Cooking']; // arrays are a reference type, which means they can be modified after being declaerd with const
+
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+
+
+
+// // for (let hobby of hobbies) {
+// //     console.log(hobby);
+// // }
+
+// // console.log(hobbies.map(hobby => 'Hobby: ' + hobby)); //map() allows you to transform your array into a new array
+// // console.log(hobbies) // the old array still remains
+
+// // hobbies.push('Programming'); // the thing stored at the variable is the pointer (stack memory), and the pointer is not changed, only the pointer points to in the heap memory
+// // console.log(hobbies)
+
+// const copiedArray = hobbies.slice(); // slice() copies an array
+// console.log(copiedArray);
+
+// // const copiedArray2 = [...hobbies]; // ... --> spread operator
+// // console.log(copiedArray2);
+
+// const copiedPerson = {...person}; // ... --> spread operator (extract elements of an object)
+// console.log(copiedPerson);
+
+// const toArray = (...args) => { // ... ---> rest operator (bundle arguments into an object)
+//     return [...args]
 // }
 
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby)); //map() allows you to transform your array into a new array
-console.log(hobbies) // the old array still remains
-
-
-
+// console.log(toArray(1,2,3,4,5,6))
